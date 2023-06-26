e0b58204d60f41079383b0edef7472b76d630213
Vijay Vikram Singh <svijayvikram00@gmail.com>
2023-06-26 09:40:59 +0530

    added normal animation on views

--- UiAnimationPractice/ViewController.swift
+++ UiAnimationPractice/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var emailView: UIView!
+    @IBOutlet weak var loginLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loginView.center.x  -= view.bounds.width
+        emailView.center.x -= view.bounds.width
+        passwordView.center.x -= view.bounds.width
+        UIView.animate(withDuration: 1) {
+            self.loginView.center.x += self.view.bounds.width
+        }
+        UIView.animate(withDuration: 1, delay: 0.3) {
+            self.emailView.center.x += self.view.bounds.width
+        }
+        UIView.animate(withDuration: 1, delay: 0.4) {
+            self.passwordView.center.x += self.view.bounds.width
+        }
+    }
+
+    override func viewDidLayoutSubviews() {
+        
+    }
 
 
+    @IBAction func loginBtnTapped(_ sender: UIButton) {
+    }
 }
 
